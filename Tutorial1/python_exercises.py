f2c01f81cabdf2d20c59cafcfa479b967b4460f8
Yihao Yang <yihaoy@Yihaos-MacBook-Pro.local>
2020-09-26 11:52:18 -0400

    Tutorial 1 python

--- Tutorial1/python_exercises.py
+++ Tutorial1/python_exercises.py
@@ -6,12 +6,14 @@ def is_odd(x):
     """
     returns True if x is odd and False otherwise
     """
+    return isinstance(x,int) and bool(x % 2)
 
 
 def is_palindrome(word):
     """
     returns whether `word` is spelled the same forwards and backwards
     """
+    return (word[::-1]==word)
 
 
 def only_odds(numlist):
@@ -20,6 +22,7 @@ def only_odds(numlist):
 
     ex: only_odds([1, 2, 3, 4, 5, 6]) -> [1, 3, 5]
     """
+    return [_ for _ in numlist if is_odd(_)]
 
 
 def count_words(text):
@@ -35,3 +38,5 @@ def count_words(text):
         {'how': 1, 'much': 1, 'wood': 1, 'would': 1, 'a': 2, 'woodchuck': 2,
         'chuck': 2, 'if': 1, 'could': 1, 'wood?': 1}
     """
+    from collections import Counter
+    return dict(Counter(text.lower().split(' ')))
